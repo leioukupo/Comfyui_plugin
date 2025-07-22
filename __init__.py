a0df1e0efe5f02edbb59de7437c11ac984904db7
leioukupo <3026187875@qq.com>
2025-07-22 23:23:28 +0800

    修复配置文件路径处理问题

--- __init__.py
+++ __init__.py
@@ -2,17 +2,17 @@ import os
 import json
 
 #check if config file exists
-if not os.path.isfile(os.path.join(os.path.dirname(__file__),"config.json")):
+if not os.path.isfile(os.path.join(os.path.realpath(__file__),"config.json")):
     #create config file
     config = {
         "autoUpdate": True,
         "branch": "dev",
         "openAI_API_Key": "sk-#########################################"
     }
-    with open(os.path.join(os.path.dirname(os.path.dirname(__file__)),"config.json"), "w") as f:
+    with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"config.json"), "w") as f:
         json.dump(config, f, indent=4)
 #load config file
-with open(os.path.join(os.path.dirname(os.path.dirname(__file__)),"config.json"), "r") as f:
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"config.json"), "r") as f:
     config = json.load(f)
 
 #check if autoUpdate is set
