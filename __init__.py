6a588e563e72d98ff72744a12d54d7c15069e24a
leioukupo <3026187875@qq.com>
2025-07-24 20:57:31 +0800

    去掉启动检查更新

--- __init__.py
+++ __init__.py
@@ -15,24 +15,6 @@ if not os.path.isfile(os.path.join(os.path.realpath(__file__),"config.json")):
 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)),"config.json"), "r") as f:
     config = json.load(f)
 
-#check if autoUpdate is set
-if "autoUpdate" not in config:
-    config["autoUpdate"] = False
-
-#check if version is set
-if "version" not in config:
-    config["branch"] = "main"
-
-if config["autoUpdate"] == True:
-    try:
-        from .Update import update as update
-        currentPath = os.path.dirname(os.path.realpath(__file__))
-        #run update/update.py to update the node class mappings
-        update.update(currentPath,branch_name=config["branch"])
-    except ImportError:
-        print("Failed to auto update `Comfyui_plugin` ")
-
-
 
 
 from .aimusic.gen_lyrics import gen_lyrics
